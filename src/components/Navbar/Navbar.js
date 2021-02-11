9c989cf8ee1edecd7732f7a5eb555accb67e49a5
kirtan09 <35260448+kirtan09@users.noreply.github.com>
2021-02-11 22:14:03 +0000

    Animation & Styling
    Adding animation to show black nav bar background when scrolling down and hide when at the top of the page.
    Add css styling for image size and layout positions

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Navbar.css";
 import Logo from "../../assets/NetflixLogo.png";
 import Avatar from "../../assets/NetflixAvatar.png";
 
 function Navbar() {
+  const [handleShow, setHandleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 150) {
+        setHandleShow(true);
+      } else {
+        setHandleShow(false);
+      }
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
   return (
-    <div className="navbar">
+    <div className={`navbar ${handleShow && "navbar-black"}`}>
       <img className="navbar-logo" src={Logo} alt="Netflix Logo" />
-      <img className="nav-avatar" src={Avatar} alt="Avatar" />
+      <img className="navbar-avatar" src={Avatar} alt="Avatar" />
     </div>
   );
 }
